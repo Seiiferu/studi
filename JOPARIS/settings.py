8f0bf8a70955b893f6271517e3e2d0e0f25d2af3
Seiiferu <jungryuuko@gmail.com>
2024-10-17 18:26:45 +0200

    Mise à jour du mode production et configuration des settings pour le déploiement

--- JOPARIS/settings.py
+++ JOPARIS/settings.py
@@ -92,7 +92,12 @@ DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
